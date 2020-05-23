49170b8ba1051ee5609dc2fa210dd4bb278cdff0
Adil Gondal <adil.gondal@yale.edu>
2020-05-23 07:21:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  males = data[:gender][:male]
+  females = data[:gender][:female]
+  all_birds = males.concat(females)
+  #p all_birds
+  hash = {}
+  all_birds.each do |name|
+    hash[name] = {:color => [], :gender => [], :lives => []}
+  end
+  color = data[:color]
+  color.each do |key,value|
+    value.each do |name|
+      hash[name][:color].push(key.to_s)
+    end
+  end
+  gender = data[:gender]
+  gender.each do |key,value|
+    value.each do |name|
+      hash[name][:gender].push(key.to_s)
+    end
+  end
+  lives = data[:lives]
+  lives.each do |key,value|
+    value.each do |name|
+      hash[name][:lives].push(key.to_s)
+    end
+  end 
+  hash
 end
